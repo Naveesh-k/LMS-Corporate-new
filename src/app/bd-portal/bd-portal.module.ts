982b0c4336dac31815790dd7f819873566b2effb
Kapil Karda <kapil.karda@gmail.com>
2021-08-14 15:43:05 +0530

    bd tech

--- src/app/bd-portal/bd-portal.module.ts
+++ src/app/bd-portal/bd-portal.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BdPortalRoutingModule } from './bd-portal-routing.module';
+import { CommonFooterComponent } from './common-footer/common-footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CommonFooterComponent
+  ],
   imports: [CommonModule, BdPortalRoutingModule],
 })
 export class BdPortalModule {}
