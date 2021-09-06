1cbc7a222d3c7b625de199ce1d10c499e7424fbb
naveesh-k <naveesh.khonde@engineermaster.in>
2021-09-06 19:14:13 +0530

    Remove dist

--- src/app/lms/structure/pages/setting/setting.component.ts
+++ src/app/lms/structure/pages/setting/setting.component.ts
@@ -8,6 +8,39 @@ import { ColorModeService } from 'src/app/service/color-mode.service';
 })
 export class SettingComponent implements OnInit {
   darkMode: boolean = false;
+  topics: any = [{
+    name: 'Road To IPO',
+    'active':false,
+  },
+  {
+    name: 'Entrepreneurship',
+    'active':false,
+  },
+  {
+    name: 'Capital Raising',
+    'active':false,
+  },
+  {
+    name: 'Trade Finance',
+    'active':false,
+  },
+  {
+    name: 'Investing',
+    'active':false,
+  },
+  {
+    name: 'CrowdFunding',
+    'active':false,
+  },
+  {
+    name: 'Business Law',
+    'active':false,
+  },
+  {
+    name: 'Merger & Acquisitions',
+    'active':false,
+  }];
+  customizeTopic : any = [];
   constructor(
     public mode: ColorModeService // dark-light
   ) {}
@@ -22,5 +55,17 @@ export class SettingComponent implements OnInit {
       }
     });
     //end dark-light
+  }
+  selectedTopics(item:any){
+    this.topics.forEach((element:any)=>{
+       if(item.name === element.name){
+        element.active = !item.active;
+        let index = this.customizeTopic.indexOf(element.name)
+        element.active ?
+          this.customizeTopic.push(element.name):
+          this.customizeTopic.splice(index, 1)
+       }
+    })
+    console.log(this.customizeTopic)
   }
 }
