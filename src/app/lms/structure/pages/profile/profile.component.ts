82c213a208eb38296e5b8491d287975227a6035f
kapilkarda <kkarda77@gmail.come>
2021-09-24 15:09:03 +0530

    changes in profile upload file

--- src/app/lms/structure/pages/profile/profile.component.ts
+++ src/app/lms/structure/pages/profile/profile.component.ts
@@ -35,6 +35,10 @@ export class ProfileComponent implements OnInit {
   imagePath: any;
   imageFile: any;
   uploadedImage: any;
+
+  //Variabel using for get profile response
+  profileResponse: any = {};
+
   constructor(
     private spinner: NgxSpinnerService,
     public _service: GobalService,
@@ -43,12 +47,10 @@ export class ProfileComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    console.log("Test ngOninit 37")
+    
     this.profileData()
-    console.log("Test ngOninit 39")
     this.updateProfileForm = this.formBuilder.group(
       {
-        // name: ['', [Validators.required]],
         fname: ['', [Validators.required]],
         lname: ['', [Validators.required]],
         email: ['', [Validators.required]],
@@ -57,7 +59,6 @@ export class ProfileComponent implements OnInit {
         country: ['', [Validators.required]],
         aboutMe: ['', [Validators.required]]
       },
-
     );
 
     // dark-light
@@ -73,28 +74,26 @@ export class ProfileComponent implements OnInit {
 
     // get User name form local storage
     this.social = localStorage.getItem('signupMode')
+
     if (this.social === 'true') {
       let getLocalStorage: any = localStorage.getItem('userDetail');
       let signUpData = JSON.parse(getLocalStorage);
       this.signupFullName = signUpData.firstName + " " + signUpData.lastName
-      this.profileImage = signUpData.photoUrl;
+      this.profile = signUpData.photoUrl;
       this.profileEmail = signUpData.email
       this.profileCompany = signUpData.provider
       this.companyName = signUpData.provider
-      console.log('profile 74', signUpData)
       console.log(this.signupFullName, this.profileImage, this.profileEmail, this.profileCompany, this.companyName, '75 profile')
       this.profileData()
     }
+
+    
     // else {
     //   let normalUserFName = localStorage.getItem('firstName')
     //   let normalUserLName = localStorage.getItem('lastName')
     //   this.profileImage = localStorage.getItem('profile');
     //   this.signupFullName = normalUserFName + ' ' + normalUserLName
     // }
-
-
-    // this.profileData()
-
   }
 
   showHide() {
@@ -140,8 +139,9 @@ export class ProfileComponent implements OnInit {
 
   updateProfile() {
 
- 
-   let request = {
+    console.log("Check profile" , this.uploadedImage)
+
+    let request = {
       // name          : this.updateProfileForm.value.name,
       contact_number: this.updateProfileForm.value.mobile,
       email: this.updateProfileForm.value.email,
@@ -150,11 +150,10 @@ export class ProfileComponent implements OnInit {
       about_me: this.updateProfileForm.value.aboutMe,
       first_name: this.updateProfileForm.value.fname,
       last_name: this.updateProfileForm.value.lname,
-      profile: this.uploadedImage.location,
+      profile: this.uploadedImage
     }
 
-    console.log("Request passed")
-
+    console.log(request)
     this._service.profileUpdate(request).subscribe(res => {
       let response = res;
       console.log('134 update profile', response)
@@ -180,67 +179,77 @@ export class ProfileComponent implements OnInit {
     this._service.uploadImage(formData).subscribe(res => {
       console.log("profile 184", res)
       this.uploadedImage = res.image
-      localStorage.setItem('profileImg', this.uploadedImage.location)
+      //localStorage.setItem('profileImg', this.uploadedImage.location)
+      localStorage.setItem('profileImg', this.uploadedImage)
     })
   }
 
   profileData() {
-    console.log('profile data api 131')
     // this.spinner.show();
     this._service.profileDataShow().subscribe(res => {
 
-      let profileObj: any = {}
       this.profileRecord = res.data
       this.profileRecord.forEach((el: any) => {
-        profileObj = el
+        this.profileResponse = el
       })
+     
+      console.log(this.profileResponse)
+
+      this.uploadedImage = this.profileResponse.profile
+
+      //Patch values data into updateProfileForm
+      this.updateProfileForm.patchValue({
+        fname: this.profileResponse.first_name,
+        lname: this.profileResponse.last_name,
+        email: this.profileResponse.email,
+        mobile: this.profileResponse.contact_number,
+        language: this.profileResponse.language,
+        country: this.profileResponse.country,
+        aboutMe: this.profileResponse.about_me
+      })
+
       // this.spinner.hide()
-      console.log(profileObj, 'social sign up 168')
-      if (this.social != 'true') {
-        // this.profileName = profileObj.first_name+' '+profileObj.last_name;
-        this.profileFName = profileObj.first_name;
-        this.profileLName = profileObj.last_name;
-        this.email = profileObj.email;
-        this.marketing = profileObj.industry;
-        this.profile = profileObj.profile;
-        this.company = profileObj.company;
-        this.contactNumber = profileObj.contact_number;
-        this.aboutMe = profileObj.about_me;
-        this.country = profileObj.country;
-        this.language = profileObj.language;
-        this.updateProfileForm.patchValue({
-          name: this.profileName,
-          email: profileObj.email,
-          mobile: this.contactNumber,
-          profile: profileObj.profile,
-          country: this.country,
-          aboutMe: this.aboutMe,
-          fname: this.profileFName,
-          lname: this.profileLName,
-          language: this.language,
+      // if (this.social != 'true') {
+      //   this.profileFName = profileObj.first_name;
+      //   this.profileLName = profileObj.last_name;
+      //   this.email = profileObj.email;
+      //   this.marketing = profileObj.industry;
+      //   this.profile = profileObj.profile;
+      //   this.company = profileObj.companyName;
+      //   this.contactNumber = profileObj.contact_number;
+      //   this.aboutMe = profileObj.about_me;
+      //   this.country = profileObj.country;
+      //   this.language = profileObj.language;
+      //   this.updateProfileForm.patchValue({
+      //     // name: this.profileName,
+      //     email: profileObj.email,
+      //     mobile: this.contactNumber,
+      //     profile: profileObj.profile,
+      //     country: this.country,
+      //     aboutMe: this.aboutMe,
+      //     fname: this.profileFName,
+      //     lname: this.profileLName,
+      //     language: this.language,
 
-        })
-      } else {
-        console.log('run', '186')
-        this.profileName = this.signupFullName;
-        this.email = this.profileEmail;
-        this.marketing = this.profileCompany;
-        this.profile = this.profileImage;
-        this.company = this.profileCompany;
-        console.log(this.profileName, 'socail 185')
-        this.updateProfileForm.patchValue({
-          name: this.profileName,
-          email: this.email,
-          mobile: '',
-          profile: this.profile,
-          language: 'English',
-          country: 'UK',
-          aboutMe: 'It is a long established fact that a reader.',
-        })
-        console.log(this.updateProfileForm.value, '201 login form')
-      }
+      //   })
+      // } else {
+      //   console.log('From Social ===>');
 
-
+      //   this.profileName = this.signupFullName;
+      //   this.email = this.profileEmail;
+      //   this.marketing = this.profileCompany;
+      //   this.profile = this.profile;
+      //   this.company = this.profileCompany;
+      //   this.updateProfileForm.patchValue({
+      //     name: this.profileName,
+      //     email: this.email,
+      //     mobile: '',
+      //     profile: this.profile,
+      //     language: 'English',
+      //     country: 'UK',
+      //     aboutMe: 'It is a long established fact that a reader.',
+      //   })
+      // }
     })
   }
 }
