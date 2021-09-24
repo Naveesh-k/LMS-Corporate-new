8589ecaf20b8de9815793835d756265654669bed
kapilkarda <kkarda77@gmail.come>
2021-09-24 19:28:25 +0530

    changes in profile 1928

--- src/app/lms/global-services/gobal.service.ts
+++ src/app/lms/global-services/gobal.service.ts
@@ -8,10 +8,10 @@ import { environment } from '../../../environments/environment';
   providedIn: 'root'
 })
 export class GobalService {
-  token :any = localStorage.getItem('token')
+  token: any = localStorage.getItem('token')
 
   globalObject: any = {
-    signup_data:'',
+    signup_data: '',
     signupType: false
   }
   globalBehaviour = new BehaviorSubject(this.globalObject);
@@ -19,65 +19,66 @@ export class GobalService {
 
   constructor(private https: HttpClient) { }
 
-  getSignUpData(data:any){
-     this.globalObject.signup_data = data;
-     this.globalBehaviour.next(this.globalObject)
-     console.log("after" , data)
-    return this.https.post(environment.lmsApiBaseUrl + 'signup',data).pipe(map(res => <any>res));
+  getSignUpData(data: any) {
+    this.globalObject.signup_data = data;
+    this.globalBehaviour.next(this.globalObject)
+    console.log("after", data)
+    return this.https.post(environment.lmsApiBaseUrl + 'signup', data).pipe(map(res => <any>res));
   }
   // getSocialLogin(data:any){
   //    console.log("after" , data)
   //   return this.https.post(environment.lmsApiBaseUrl + 'socialLogin',data).pipe(map(res => <any>res));
   // }
-  getSocialLogin(data:any){
-     console.log("after" , data)
-    return this.https.post(environment.lmsApiBaseUrl + 'social_login',data).pipe(map(res => <any>res));
+  getSocialLogin(data: any) {
+    console.log("after", data)
+    return this.https.post(environment.lmsApiBaseUrl + 'social_login', data).pipe(map(res => <any>res));
   }
-  getLinkedInLogin(data:any){
-    console.log("after" , data)
-   return this.https.post(environment.lmsApiBaseUrl + 'linkedLogin',data).pipe(map(res => <any>res));
-  }
-
-  getSignUpEmail(data:any){
-    console.log("after" , data)
-    return this.https.post(environment.lmsApiBaseUrl + 'signup',data).pipe(map(res => <any>res));
-  }
-  postFacebookLogin(data:any){
-    console.log("after" , data)
-    return this.https.post(environment.lmsApiBaseUrl + 'fbLogin',data).pipe(map(res => <any>res));
+  getLinkedInLogin(data: any) {
+    console.log("after", data)
+    return this.https.post(environment.lmsApiBaseUrl + 'linkedLogin', data).pipe(map(res => <any>res));
   }
 
-  checkSignupType(){
+  getSignUpEmail(data: any) {
+    console.log("after", data)
+    return this.https.post(environment.lmsApiBaseUrl + 'signup', data).pipe(map(res => <any>res));
+  }
+  postFacebookLogin(data: any) {
+    console.log("after", data)
+    return this.https.post(environment.lmsApiBaseUrl + 'fbLogin', data).pipe(map(res => <any>res));
+  }
+
+  checkSignupType() {
     this.globalObject.signupType = true;
     this.globalBehaviour.next(this.globalObject);
-    localStorage.setItem('signupType' , 'true')
+    localStorage.setItem('signupType', 'true')
   }
 
-  uploadImage(data:any){
-    let headers :any = new HttpHeaders({
+  uploadImage(data: any) {
+    let headers: any = new HttpHeaders({
       // Authorization: this.token,
       mimeType: 'multipart/form-data'
     });
-    console.log(headers,"profile update 57")
+    console.log(headers, "profile update 57")
 
-     return this.https.post(environment.lmsApiBaseUrl + 'upload',data ,{headers}).pipe(map(res => <any>res));
-   }
+    return this.https.post(environment.lmsApiBaseUrl + 'upload', data, { headers }).pipe(map(res => <any>res));
+  }
 
-   profileDataShow(){
-    let headers :any = new HttpHeaders({
+  profileDataShow() {
+    console.log("Api hit successfully global.service.ts 67")
+    let headers: any = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': this.token,
     });
-     return this.https.get(environment.lmsApiBaseUrl + 'showProfile',{headers}).pipe(map(res => <any>res));
+    return this.https.get(environment.lmsApiBaseUrl + 'showProfile', { headers }).pipe(map(res => <any>res));
 
-   }
+  }
 
-   profileUpdate(data:any){
+  profileUpdate(data: any) {
     console.log("profile update 76")
-   let headers :any = new HttpHeaders({
-     'Content-Type': 'application/json',
-     'Authorization': this.token,
-   });
-   return this.https.put(environment.lmsApiBaseUrl + 'updateUserProfile',data,{headers}).pipe(map(res => <any>res));
+    let headers: any = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': this.token,
+    });
+    return this.https.put(environment.lmsApiBaseUrl + 'updateUserProfile', data, { headers }).pipe(map(res => <any>res));
   }
 }
