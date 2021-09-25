45b442a0ce494878289d6cd2c9aa8cafa524ea47
kapilkarda <kkarda77@gmail.come>
2021-09-25 14:59:35 +0530

    chnages

--- src/app/lms/global-services/gobal.service.ts
+++ src/app/lms/global-services/gobal.service.ts
@@ -73,22 +73,14 @@ export class GobalService {
 
   }
 
-  // profileUpdate(data: any) {
-  //   console.log("profile update 76")
-  //   let headers: any = new HttpHeaders({
-  //     'Content-Type': 'application/json',
-  //     'Authorization': this.token,
-  //   });
-  //   return this.https.put(environment.lmsApiBaseUrl + 'updateUserProfile', data, { headers }).pipe(map(res => <any>res));
-  // }
-
   profileUpdate(data: any) {
-    console.log("profile update 76")
+    console.log("Token", this.token)
+
     let headers: any = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': this.token,
     });
-    console.log("Header passed 91 global service", )
-    return this.https.put(environment.lmsApiBaseUrl + 'updateUserProfile', data, { headers });
+    return this.https.put(environment.lmsApiBaseUrl + 'updateUserProfile', data, { headers }).pipe(map(res => <any>res));
   }
+
 }
