3cb34c7bbfdcbdd087b129b826507c0937791ae7
naveesh-k <naveesh.khonde@engineermaster.in>
2021-09-29 10:57:58 +0530

    Remove dist

--- src/app/lms/auth/component/sign-up/sign-up.component.ts
+++ src/app/lms/auth/component/sign-up/sign-up.component.ts
@@ -111,8 +111,8 @@ export class SignUpComponent implements OnInit {
     }
 
     this.registerForm = this.formBuilder.group({
-      firstName: this.checkSignUptype ? signUpData.firstName : '',
-      lastName: this.checkSignUptype ? signUpData.lastName : '',
+      first_name: this.checkSignUptype ? signUpData.firstName : '',
+      last_name: this.checkSignUptype ? signUpData.lastName : '',
       email: this.checkSignUptype ? signUpData.email : '',
       profile: '',
       password: '',
