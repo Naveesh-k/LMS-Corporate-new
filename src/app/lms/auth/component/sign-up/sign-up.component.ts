a39659ae3e3f8aeaaeb8aee8d55dae4feab6f67f
naveesh-k <naveesh.khonde@engineermaster.in>
2021-09-30 15:42:23 +0530

    Remove dist

--- src/app/lms/auth/component/sign-up/sign-up.component.ts
+++ src/app/lms/auth/component/sign-up/sign-up.component.ts
@@ -132,7 +132,7 @@ export class SignUpComponent implements OnInit {
       market: '',
       provider: '',
       contactNumber: '',
-      customizeTopic: [],
+      // customizeTopic: [],
     });
 
     // -----------------------------------------------------------------
